2a5959b87a31da50f9696a1d415da0e7748e1cf9
blavikensbutcher <luckertheory@gmail.com>
2024-07-20 13:07:04 +0300

    task service

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -1,31 +1,32 @@
-import {axiosWithAuth} from "@/api/interceptors";
-import {ITaskResponse, TypeTaskFormState} from "@/types/task.types";
+import { ITaskResponse, TypeTaskFormState } from '@/types/task.types'
+
+import { axiosWithAuth } from '@/api/interceptors'
 
 class TaskService {
-    private BASE_URL = "/users/tasks"
+	private BASE_URL = '/users/tasks'
 
-    async getAllTasks() {
-        const response = await axiosWithAuth.get<ITaskResponse[]>(this.BASE_URL)
-        return response
-    }
+	async getAllTasks() {
+		const response = await axiosWithAuth.get<ITaskResponse[]>(this.BASE_URL)
+		return response
+	}
 
-    async createTask(data: TypeTaskFormState){
-        const response = await axiosWithAuth.post(this.BASE_URL, data)
-        return response
-    }
+	async createTask(data: TypeTaskFormState) {
+		const response = await axiosWithAuth.post(this.BASE_URL, data)
+		return response
+	}
 
-    async updateTask(taskId: string, data: TypeTaskFormState){
-        const response = await axiosWithAuth.patch(`${this.BASE_URL}/${taskId}`, data)
-        return response
-    }
+	async updateTask(taskId: string, data: TypeTaskFormState) {
+		const response = await axiosWithAuth.patch(
+			`${this.BASE_URL}/${taskId}`,
+			data
+		)
+		return response
+	}
 
-
-    async deleteTask(taskId:string) {
-        const response = await axiosWithAuth.delete(`${this.BASE_URL}/${taskId}`)
-        return response
-    }
-
-
+	async deleteTask(taskId: string) {
+		const response = await axiosWithAuth.delete(`${this.BASE_URL}/${taskId}`)
+		return response
+	}
 }
 
 export const taskService = new TaskService()
